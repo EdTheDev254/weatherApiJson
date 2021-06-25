733b18603ff007c4c4851982e883cc31e6a7bef0
EdTheDev254 <86492702+EdTheDev254@users.noreply.github.com>
2021-06-25 21:57:16 +0300

    Create JsonCollector.py

---
+++ JsonCollector.py
@@ -0,0 +1,19 @@
+import requests
+
+#make a function to collect the json file 
+#with a parameter of the city's or capital city's
+#name
+
+def get_json (city) :
+	
+	weather_key = "76b9baa188dca2b6a27a92c2d09404c8"
+	parameters = {"APPID" :weather_key,"q" :city, "units" :"metric"}
+	url = "http://api.openweathermap.org/data/2.5/weather"
+	response_data = requests.get(url, params = parameters) 
+	json_output = response_data.json()
+	
+	return json_output
+
+city_name=input("Enter country's or capital city's name:" )
+print("Json data below:\n")
+print(get_json(city_name))
